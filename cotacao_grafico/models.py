3a2b62d8d9c3dd5588623f179e6aa098ea8110e3
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-07-03 12:00:37 -0300

    Método retorna listas para renderizar

--- cotacao_grafico/models.py
+++ cotacao_grafico/models.py
@@ -8,5 +8,28 @@ class Precos(models.Model):
     moeda = models.CharField(max_length=30)
     data = models.DateField()
 
+    def getDict(moeda, start_date, end_date):
+        '''Gera dados para renderizar grafico
+            :param moeda: str
+            :param start_date: str
+            :param end_date: str
+
+            :return dicionario com lista de precos, lista de datas e moeda
+        '''
+        listaPrecos = []
+        listaDatas = []
+        dict1 = dict()
+        precos = Precos.objects.filter(moeda=moeda).filter(data__range=[start_date, end_date]).order_by('data')
+        for preco in precos:
+            listaPrecos.append(preco.preco)
+            listaDatas.append(preco.data)
+
+        dict1['moeda'] = moeda
+        dict1['precos'] = listaPrecos
+        dict1['data'] = listaDatas
+
+        return dict1
+
+
     def __str__(self):
         return str(self.data)  + ' - ' + self.moeda + ' - ' + str(self.preco)
